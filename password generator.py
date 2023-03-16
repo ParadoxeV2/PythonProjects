4707486c15183b2eb888ce618d45468015373742
Paradoxe <106479771+ParadoxeV2@users.noreply.github.com>
2023-03-16 22:19:22 +0100

    Revert "Create password generator.py"
    This reverts commit d68a957714cba4c882a7d7194995d9b10a9e59ea.

--- password generator.py
+++
@@ -1,5 +0,0 @@
-from random import *
-from random import choice
-liste = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",1,2,3,4,5,6,7,8,9,0]
-for x in range(randint(8,12)):
-    print(choice(liste),end="")
